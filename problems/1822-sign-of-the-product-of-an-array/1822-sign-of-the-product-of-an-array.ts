8b42742244d399a13b1b7ea131a0f19aeca3d8ce
Anirbandasjoy <joy600508@gmail.com>
2025-04-30 11:46:56 +0600

    update code

--- problems/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.ts
+++ problems/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.ts
@@ -1,27 +1,31 @@
 function arraySign(nums: number[]): number {
-  function signFunc(x: number): number {
+  function singFunc(x: number): number {
     let value = 0;
     if (x === 0) {
       value = 0;
-    } else if (x > 1) {
+    } else if (x >= 1) {
       value = 1;
-    } else if (x < 1) {
+    } else {
       value = -1;
     }
     return value;
   }
+
   let n = 0;
   while (nums[n] !== undefined) {
     n++;
   }
-  let mul = 1;
+
+  let multi = 1;
+
   for (let i = 0; i < n; i++) {
-    const tem = nums[i] * mul;
-    mul = tem;
+    let temp = multi * nums[i];
+    multi = temp;
   }
-  return signFunc(mul);
+  return singFunc(multi);
 }
 
 console.log(arraySign([-1, -2, -3, -4, 3, 2, 1]));
 console.log(arraySign([1, 5, 0, 2, -3]));
 console.log(arraySign([-1, 1, -1, 1, -1]));
+console.log(arraySign([1, 1]));
